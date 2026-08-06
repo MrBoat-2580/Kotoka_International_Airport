3098a1b07d6706cd867a1d6aee8c6fafe6dd262d
NIO_81720 <nadiasparks843@gmail.com>
2026-08-06 22:05:32 +0000

    Added the information needed in the README File, Initial Commit

--- KIA.py
+++ KIA.py
@@ -1,1 +1,13 @@
-print("KOTOKA AIRPORT")
+#Airport Check-in & Boarding Management System
+
+while exit== "no":
+    print()
+    print("=========================================")
+    print("KOTOKA AIRPORT CHECK-IN SYSTEM")
+    print("=========================================")
+    print()
+    
+#     1. Check In Passenger
+# 2. View Daily Statistics
+# 3. View Flight Summary
+# 4. Close Check-in
---
+++ README.md
@@ -0,0 +1,8 @@
+##Information to be included in the README
+
+Group name
+   * Group members
+   * Student IDs
+   * GitHub usernames
+   * Description of the system
+   * Instructions for running the program.
