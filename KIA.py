9cdf857416b9c744498a6c77509dc9210c70cf31
NIO_81720 <nadiasparks843@gmail.com>
2026-08-07 18:58:40 +0000

    Printing main menu and taking input from passenger

--- KIA.py
+++ KIA.py
@@ -1,13 +1,38 @@
-#Airport Check-in & Boarding Management System
+###Airport Check-in & Boarding Management System
 
-while exit== "no":
-    print()
-    print("=========================================")
-    print("KOTOKA AIRPORT CHECK-IN SYSTEM")
-    print("=========================================")
-    print()
-    
-#     1. Check In Passenger
-# 2. View Daily Statistics
-# 3. View Flight Summary
-# 4. Close Check-in
+# close_checkin = "Yes"
+# while close_checkin == "no":
+#     close_checkin = "Yes"
+print()
+print("=========================================")
+print("KOTOKA AIRPORT CHECK-IN SYSTEM")
+print("=========================================")
+print()
+print("1. Check In Passenger")
+print("2. View Daily Statistics")
+print("3. View Flight Summary")
+print("4. Close Check-in")
+print()
+print()
+print()
+
+#Feature 1
+print("______________________")
+print("- Passenger Check-in -")
+print("______________________")
+
+passenger_name= input("Enter the passenger's full name: ")
+passport_number= input("Enter the passenger's passport number: ")
+nationality= input("Enter the passengers nationality: ")
+age= input("Enter the passenger's age: ")
+destination= input("Where is the passenger headed?: ")
+ticket_class= input("What is the class of the passengers ticket?: ")
+number_of_bags= int(input("How many bags is the passenger carrying?: "))
+close_checkin=input("Close Check-in?: Yes/No").lower()
+
+# for i in range: number_of_bags
+# bag_weight[i]= int(input(f"Enter the weight of bag {i}: "))
+
+
+
+
