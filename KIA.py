87eb5a7dba6eb79c94c12b1f1664c082c68f1917
MrBoat-2580 <clementboateng2580@gmail.com>
2026-08-08 14:21:00 +0000

    FINAL PUSH

--- KIA.py
+++ KIA.py
@@ -1,6 +1,18 @@
 print("==========KOTOKA INTERNATIONAL AIRPORT CHECK-IN SYSTEM================")\
 
 passenger_details={}
+statistics={}
+children = 0
+adult=0
+economy=0
+business=0
+first_class=0
+passengers_inspection=0
+total_bags=0
+total_passengers=0
+total_weight=0
+#average_weightperP=total_weight/total_passengers
+#average_bagperP=total_bags/total_passengers
 
 print("1. Check In Passenger\n2. View Daily Statistics\n3. View Flight Summary\n4. Close Check-In")
 
@@ -8,10 +20,15 @@ print("1. Check In Passenger\n2. View Daily Statistics\n3. View Flight Summary\n
 while True:
     choice=input("Enter Choice: ") 
     if choice == '1':
+        total_passengers+=1
         name=input("Enter Passenger Name: ")
         id=input("Enter Passenger ID: ")
         nationality=input("Enter Passenger Nationality: ")
         age=int(input("Enter Passenger Age: "))
+        if age<18:
+            children+=1
+        else:
+            adult+=1
         destination=input("Enter Destination: ")
 
         print(" ")
@@ -19,6 +36,14 @@ while True:
 
         print("1. First Class\n2. Economy Class\n3. Business Class  ")
         ticket_class=int(input("Enter Ticket Class: "))
+        if ticket_class == '1':
+            first_class+=1
+        elif ticket_class =='2':
+            economy +=1
+        elif ticket_class =='3':
+            business+=1
+        else:
+            print("Invalid Ticket Class")
         print(" ")
         
         print("=====BAGGAGE SECTION=====")
@@ -26,13 +51,14 @@ while True:
         luggage=int(input("Enter Number of Bags: "))
         if luggage < 0:
             print("Invalid Number of Bags")
+        total_bags+=luggage
         count=1
         
         total_charge1=0
         total_charge2=0
         total_charge3=0
         total_charge=total_charge3+total_charge2+total_charge1
-        total_weight=0
+        #total_weight=0
         for i in range(1,luggage+1):
             weight= int(input(f"Enter Weight of Bag {count}: "))
             total_weight+=weight
@@ -97,7 +123,8 @@ while True:
             eligibility_to_board= "NO"
         else:
             eligibility_to_board= "YES"
-
+        if security1 or security2:
+            passengers_inspection+=1
 
         passenger_details["NAME"]=name
         passenger_details["PASSPORT NUMBER"]=id
@@ -110,7 +137,34 @@ while True:
         passenger_details["TOTAL LUGGAGE WEIGHT"]=total_weight
         passenger_details["SECURITY STATUS"]=security_status
         passenger_details["ElIGIBILITY TO BOARD"]=eligibility_to_board
+        statistics["CHILDREN"]=children
+        statistics["ADULTS"]=adult
+        statistics["ECONOMY CLASS"]=economy
+        statistics["FIRST CLASS"]=first_class
+        statistics["BUSINESS CLASS"]=business
+        statistics["PASSENGER SENT FOR INSPECTION"]=passengers_inspection
+        statistics["TOTAL CHARGES"]=total_charge
+        statistics["TOTAL BAGS PROCESSED"]=total_bags
+    #average_weightperP=total_weight/total_passengers
+    #average_bagperP=total_bags/total_passengers
         print(passenger_details)
-
-
-            
+    elif choice == '2':
+        print(statistics)
+    elif choice =='3':
+        print("=================")
+        print("END OF DAY REPORT")
+        print("=================")
+        print(' ')
+        print(f"PASSENGER PROCESSED: {total_passengers}" )
+        print(f"NUMBER OF CHILDREN: {children}")
+        print(f"TOTAL BAGS: {total_bags}")
+        average_weightperP=total_weight/total_passengers
+        average_bagperP=total_bags/total_passengers
+        
+        print(f"AVERAGE WEIGHT PER PASSENGER: {average_weightperP}")
+        print(f"AVERAGE BAG PER PASSENGER: {average_bagperP}")
+        print(f"AVERAGE REVENUE FOR LUGGAGE: {total_charge}")
+    elif choice =='4':
+        userchoice=input("ARE YOU SURE YOU WANT TO CLOSE TODAY'S CHECK-IN? (Y/N): ").capitalize()
+        if userchoice == 'Y':
+            break
